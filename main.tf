94bffe307f47199ac3a8cc636c13718250cd716a
Mateusz Zdrenka <zdrenka.mateusz@gmail.com>
2026-03-14 19:02:14 +0100

    feat(terraform): Parameterize server port and update user data script

--- main.tf
+++ main.tf
@@ -15,6 +15,14 @@ provider "aws" {
 
 }
 
+variable "server_port" {
+
+  description = "Port number for the HTTP server"
+  type        = number
+  default     = 8080
+
+}
+
 resource "aws_instance" "example" {
   ami                         = "ami-04505e74c0741db8d"
   instance_type               = "t3.micro"
@@ -22,7 +30,7 @@ resource "aws_instance" "example" {
   user_data                   = <<-EOF
               #!/bin/bash
               echo "Witaj, świecie" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
   user_data_replace_on_change = true
   tags = {
@@ -33,10 +41,15 @@ resource "aws_instance" "example" {
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+}
+
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "Public IP address of the EC2 instance"
 }
